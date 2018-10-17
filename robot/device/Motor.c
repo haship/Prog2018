fec2daedf0bfd13e057b864e662eb44965e8e45c
haship <haship@gmail.com>
2018-10-17 22:25:03 +0900

    Motorクラス実装

--- robot/device/Motor.c
+++ robot/device/Motor.c
@@ -6,3 +6,28 @@
  */
 
 #include "Motor.h"
+
+void Motor_init(Motor* this, motor_port_t port, motor_type_t type){
+    this->mPort = port;
+    this->mType = type;
+}
+
+int32_t Motor_getAngle(Motor* this){
+    return ev3_motor_get_counts(this->mPort);
+}
+
+void Motor_resetAngle(Motor* this){
+    ev3_motor_reset_counts(this->mPort);
+}
+
+void Motor_setPower(Motor* this, int power){
+    ev3_motor_set_power(this->mPort, power);
+}
+
+int Motor_getPower(Motor* this){
+    return ev3_motor_get_power(this->mPort);
+}
+
+void Motor_stop(Motor* this){
+    ev3_motor_stop(this->mPort, true);
+}
--- robot/device/Motor.h
+++ robot/device/Motor.h
@@ -8,6 +8,18 @@
 #ifndef ROBOT_DEVICE_MOTOR_H_
 #define ROBOT_DEVICE_MOTOR_H_
 
+#include "ev3api.h"
 
+typedef struct Motor {
+    motor_type_t mType;
+    motor_port_t mPort;
+} Motor;
+
+void Motor_init(Motor* this, motor_port_t port, motor_type_t type);
+int32_t Motor_getAngle(Motor* this);
+void Motor_resetAngle(Motor* this);
+void Motor_setPower(Motor* this, int power);
+int Motor_getPower(Motor* this);
+void Motor_stop(Motor* this);
 
 #endif /* ROBOT_DEVICE_MOTOR_H_ */
